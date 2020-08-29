7eb5441230b26dc4822a95b485c20484170ce3b3
pshdev789 <68139465+pshdev789@users.noreply.github.com>
2020-08-29 16:36:09 +0530

    Initial Commit for todo-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Comment {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <h2>MERN-Stack Todo App</h2>
+        </div>
+      </Router>
+    );
+  }
+
 }
 
 export default App;
