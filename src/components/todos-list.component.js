a793c52245a48cc0d625121a4e293b10ed6865d7
pshdev789 <68139465+pshdev789@users.noreply.github.com>
2020-09-05 07:19:49 +0530

    Update the ToDoList to add the functionalities of the Edit todo list.

--- src/components/todos-list.component.js
+++ src/components/todos-list.component.js
@@ -3,15 +3,19 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 const Todo = props =>(
+
     <tr>
-    <td>{props.todo.todo_description}</td>
-    <td>{props.todo.todo_responsible}</td>
-    <td>{props.todo.todo_priority}</td>
-    <td>
-        <Link to={"/edit/"+props.todo._id}>Edit</Link>
-    </td>
-</tr>
+        <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_description}</td>
+        <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_responsible}</td>
+        <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_priority}</td>
+
+        <td>
+            <Link to={"/edit/"+props.todo._id}>Edit</Link>
+        </td>
+    </tr>
+
 )
+
 
 
 export default class TodosList extends Component {
